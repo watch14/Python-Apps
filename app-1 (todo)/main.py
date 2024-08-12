40281b18b9bc9222c88fb2b1da2456a670ebd8b5
watch14 <watchman1414@gmail.com>
2024-08-12 12:17:14 +0100

    todo match case

--- app-1 (todo)/main.py
+++ app-1 (todo)/main.py
@@ -1,5 +1,6 @@
 print("Welcom to your Todo List!")
 tasks= []
+
 while True :
     user_action = input("Type add, show or exit: ")
     
@@ -11,6 +12,5 @@ while True :
             print(tasks)
         case 'exit':
             break
-            
 
 print(f"******\nYou quit your Todo List.\nThese are your Tasks: {tasks} \n******")
