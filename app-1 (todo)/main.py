3ce8e23fab9312e5647f1da06c8d9b79d0b980e9
watch14 <watchman1414@gmail.com>
2024-08-12 12:47:07 +0100

    implementing edit

--- app-1 (todo)/main.py
+++ app-1 (todo)/main.py
@@ -1,8 +1,9 @@
 print("Welcom to your Todo List!")
-tasks= []
+tasks= ['eat', 'sleep', 'repeat']
 
 while True :
     user_action = input("Type add, show or exit: ")
+    user_action = user_action.strip().lower()
     
     match user_action:
         case 'add':
@@ -10,7 +11,13 @@ while True :
             tasks.append(todo)
         case 'show':
             print(tasks)
+        case 'edit':
+            nb = int(input("Enter the nomber of the task to edit: "))
+            if nb >= len(tasks):
+                print(f"Out of range the number should be no more than {len(tasks)}")
+                
+            print(tasks[nb])
         case 'exit':
             break
 
-print(f"******\nYou quit your Todo List.\nThese are your Tasks: {tasks} \n******")
+print(f"******\nYou quit your Todo List!!\nThese are your Tasks: {tasks} \n******")
