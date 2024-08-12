cf8f1fa34e06ebe8e0ae50a2224ff7b42289cad8
watch14 <watchman1414@gmail.com>
2024-08-12 13:02:15 +0100

    implemented edit + show with numbers

--- app-1 (todo)/main.py
+++ app-1 (todo)/main.py
@@ -2,22 +2,31 @@ print("Welcom to your Todo List!")
 tasks= ['eat', 'sleep', 'repeat']
 
 while True :
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip().lower()
     
     match user_action:
         case 'add':
-            todo = input("Enter a Task: ")
+            todo = input("Enter a Task: ").strip()
             tasks.append(todo)
+            
         case 'show':
-            print(tasks)
+            for index, item in enumerate(tasks):
+                print(index+1, item)
+            
         case 'edit':
-            nb = int(input("Enter the nomber of the task to edit: "))
-            if nb >= len(tasks):
-                print(f"Out of range the number should be no more than {len(tasks)}")
+            nb = int(input("Enter the nomber of the task to edit: ").strip())
+            if 1 <= nb <= len(tasks):
+                edited = input(f"Edit {tasks[nb - 1]}: ")
+                tasks[nb - 1] = edited
+            else:
+                print(f"Out of range the number should be between 1 and {len(tasks)}")
                 
-            print(tasks[nb])
+
         case 'exit':
             break
+        
+        case _:
+            print("Invalid action!")
 
-print(f"******\nYou quit your Todo List!!\nThese are your Tasks: {tasks} \n******")
+print(f"******\nYou quit your Todo List!!\nThese are your {len(tasks)} Tasks: {tasks}  \n******")
