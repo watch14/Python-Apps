02820264ba84dd4bcd2bf5351250bc68ec91cf6b
watch14 <watchman1414@gmail.com>
2024-08-12 14:30:41 +0100

    store todos in a file

--- app-1 (todo)/main.py
+++ app-1 (todo)/main.py
@@ -1,39 +1,54 @@
 print("Welcom to your Todo List!")
-tasks= ['eat', 'sleep', 'repeat']
+
 
 while True :
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip().lower()
     
+    f= open("todos.txt", "r")
+    todos = f.readlines()
+    f.close()
+            
     match user_action:
         case 'add':
-            todo = input("Enter a Task: ").strip()
-            tasks.append(todo)
+            todo = input("Enter a Task: ").strip().title() + '\n'
+
+            todos.append(todo)
+            
+            f = open("todos.txt", "w")
+            f.writelines(todos)
+            f.close()
             
         case 'show':
-            for index, item in enumerate(tasks):
-                print(f"{index+1}. {item}")
+
+            print("Your todos:")
+            for index, item in enumerate(todos):
+                print(f"{index + 1}. {item.strip()}")
+
             
         case 'edit':
             nb = int(input("Enter the number of the task to edit: ").strip())
-            if 1 <= nb <= len(tasks):
-                edited = input(f"Edit {tasks[nb - 1]}: ")
-                tasks[nb - 1] = edited
+            if 1 <= nb <= len(todos):
+                edited = input(f"Edit {todos[nb - 1]}: ")
+                todos[nb - 1] = edited
             else:
-                print(f"Out of range the number should be between 1 and {len(tasks)}")
+                print(f"Out of range the number should be between 1 and {len(todos)}")
                 
         case 'complete':
             nb = int(input("Enter the number of the task that u Completed: ").strip())
-            if 1 <= nb <= len(tasks):
-                print(f"Task {tasks[nb - 1]} is Complete Well Done!!!")
-                tasks.pop(nb - 1)
+            if 1 <= nb <= len(todos):
+                print(f"Task {todos[nb - 1]} is Complete Well Done!!!")
+                todos.pop(nb - 1)
             else:
-                print(f"Out of range the number should be between 1 and {len(tasks)}")
+                print(f"Out of range the number should be between 1 and {len(todos)}")
 
         case 'exit':
             break
         
         case _:
             print("Invalid action!")
-
-print(f"******\nYou quit your Todo List!!\nThese are your {len(tasks)} Tasks: {tasks}  \n******")
+            
+print("***************")
+print(f"You quit your Todo List!!\n")
+print(f"These are your {len(todos)} todos: {todos}")
+print("***************")
