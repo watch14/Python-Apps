b7853ced81df11c93617c9f7bf0a3aae6ddcd1be
watch14 <watchman1414@gmail.com>
2024-08-12 13:38:18 +0100

    implemented compete the task

--- app-1 (todo)/main.py
+++ app-1 (todo)/main.py
@@ -12,16 +12,23 @@ while True :
             
         case 'show':
             for index, item in enumerate(tasks):
-                print(index+1, item)
+                print(f"{index+1}. {item}")
             
         case 'edit':
-            nb = int(input("Enter the nomber of the task to edit: ").strip())
+            nb = int(input("Enter the number of the task to edit: ").strip())
             if 1 <= nb <= len(tasks):
                 edited = input(f"Edit {tasks[nb - 1]}: ")
                 tasks[nb - 1] = edited
             else:
                 print(f"Out of range the number should be between 1 and {len(tasks)}")
                 
+        case 'complete':
+            nb = int(input("Enter the number of the task that u Completed: ").strip())
+            if 1 <= nb <= len(tasks):
+                print(f"Task {tasks[nb - 1]} is Complete Well Done!!!")
+                tasks.pop(nb - 1)
+            else:
+                print(f"Out of range the number should be between 1 and {len(tasks)}")
 
         case 'exit':
             break
