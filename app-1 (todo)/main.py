76d894592ca44a5434000b894cfec774e42cb016
watch14 <watchman1414@gmail.com>
2024-08-13 17:07:26 +0100

    seprated the functions from the main file

--- app-1 (todo)/main.py
+++ app-1 (todo)/main.py
@@ -1,27 +1,4 @@
-def getTodos(file):
-    """ Return file content - todos - taks in a List """
-    with open(file, "r") as f:
-        todos =  f.readlines()
-        return todos
-
-
-def saveFile(file, todos_arg):
-    """ Save / Write the content/todos in the file """
-    with open(file, "w") as f:
-        f.writelines(todos_arg)
-
-
-def printToods(file):
-    """
-    get the contetnt of the file 
-    and Print each line enumerated 
-    """
-    todos = getTodos(file)
-    print("Your todos:")
-    for index, item in enumerate(todos):
-        print(f"{index + 1}. {item.strip("\n")}")
-
-
+from functions import *
 print("-------------------------")
 print("Welcom to your Todo List!")
 print("-------------------------")
