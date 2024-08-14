82d522040a416ba2ca18449075fe85fdf3ae0611
watch14 <watchman1414@gmail.com>
2024-08-14 16:43:43 +0100

    Simple gui To-Dos✅ + theme

--- app-1 (todo)/simple_gui_todo.py
+++ app-1 (todo)/simple_gui_todo.py
@@ -2,65 +2,78 @@ import PySimpleGUI as sg
 from functions import *
 from functions_gui import *
 
-
 filePath = "files/todos.txt"
-
+fonts = ('Helvetica', 10)
+sg.theme("Reds")
 # Define the layout
 layout = [
-    [sg.Text(currentTime())],
-    [sg.Text("To-Do List")],
+    [sg.Text("", key='-TIME-')],
+    [sg.Text("My To-Do List")],
     [sg.InputText(key='-TODO-'), sg.Button("Add")],
     [sg.Listbox(values=getTodos(filePath), key='-TODOS-',
                 enable_events=True, size=[49, 10])],
     [sg.Button("Edit"), sg.Button("Complete")],
 ]
 
-
 # Create the window
-window = sg.Window("To-Do List", layout, font=('Helvetica', 10))
+window = sg.Window("To-Do List", layout, font=fonts)
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    
+    # Check if the window was closed
+    if event == sg.WIN_CLOSED:
+        break
+
+    time = currentTime()
+    
+    # Ensure that the window is still open before updating
+    if window:
+        window['-TIME-'].update(value=time)
     
     todos = getTodos(filePath)
-    
-    print("event:",event)
-    print("values:",values)
 
     if event == "Add":
-        # Handle the Add button click
         todo_item = values['-TODO-'].strip().title() + "\n"
         todos.append(todo_item)
         
         saveFile(filePath, todos)
-        window['-TODOS-'].update(values=todos)
-        
+        if window:
+            window['-TODOS-'].update(values=todos)
+            window['-TODO-'].update(value='')
         
     elif event == "Edit":
-        todoToEdit = values["-TODOS-"][0]
-        todoEdited= values['-TODO-'].strip().title() + "\n"
-        
-        i = todos.index(todoToEdit)
-        todos[i] = todoEdited
-        
-        saveFile(filePath, todos)
-        window['-TODOS-'].update(values=todos)
-        
+        try:
+            todoToEdit = values["-TODOS-"][0]
+            todoEdited = values['-TODO-'].strip().title() + "\n"
+            
+            i = todos.index(todoToEdit)
+            todos[i] = todoEdited
+            
+            saveFile(filePath, todos)
+            if window:
+                window['-TODOS-'].update(values=todos)
+                window['-TODO-'].update(value='')
+        except IndexError:
+            sg.popup("Select a task to Edit!", font=fonts)
         
     elif event == "Complete":
-        todoToEdit = values["-TODOS-"][0]
-        
-        i = todos.index(todoToEdit)
-        todos.pop(i)
-        
-        saveFile(filePath, todos)
-        
-        window['-TODOS-'].update(values=todos)
+        try:
+            todoToEdit = values["-TODOS-"][0]
             
+            i = todos.index(todoToEdit)
+            todos.pop(i)
+            
+            saveFile(filePath, todos)
+            
+            if window:
+                window['-TODOS-'].update(values=todos)
+                window['-TODO-'].update(value='')
+        except IndexError:
+            sg.popup("Select a task to Complete!", font=fonts)
+         
     elif event == "-TODOS-":
-        window['-TODO-'].update(value=values["-TODOS-"][0].strip())
-        
-    elif event == sg.WIN_CLOSED:
-        break
+        if values["-TODOS-"]:
+            window['-TODO-'].update(value=values["-TODOS-"][0].strip())
 
 window.close()
--- app-1 (todo)/tempCodeRunnerFile.py
+++ app-1 (todo)/tempCodeRunnerFile.py
@@ -1,1 +1,78 @@
-                todos[i] = todoEdited
+import PySimpleGUI as sg
+from functions import *
+from functions_gui import *
+
+
+filePath = "files/todos.txt"
+fonts = ('Helvetica', 10)
+# Define the layout
+layout = [
+    [sg.Text("", key='-TIME-')],
+    [sg.Text("My To-Do List")],
+    [sg.InputText(key='-TODO-'), sg.Button("Add")],
+    [sg.Listbox(values=getTodos(filePath), key='-TODOS-',
+                enable_events=True, size=[49, 10])],
+    [sg.Button("Edit"), sg.Button("Complete")],
+]
+
+
+# Create the window
+window = sg.Window("To-Do List", layout, font=fonts)
+
+while True:
+    event, values = window.read(timeout=100)
+    time = currentTime()
+    window['-TIME-'].update(value=time)
+    
+    todos = getTodos(filePath)
+    
+    # print("event:",event)
+    # print("values:",values)
+
+    if event == "Add":
+        # Handle the Add button click
+        todo_item = values['-TODO-'].strip().title() + "\n"
+        todos.append(todo_item)
+        
+        saveFile(filePath, todos)
+        window['-TODOS-'].update(values=todos)
+        window['-TODO-'].update(value='')
+        
+        
+    elif event == "Edit":
+        try:
+            todoToEdit = values["-TODOS-"][0]
+            todoEdited= values['-TODO-'].strip().title() + "\n"
+            
+            i = todos.index(todoToEdit)
+            todos[i] = todoEdited
+            
+            saveFile(filePath, todos)
+            window['-TODOS-'].update(values=todos)
+            window['-TODO-'].update(value='')
+        except IndexError:
+            sg.popup("Select a task to Edit!", font=fonts)
+        
+        
+    elif event == "Complete":
+        try:
+            todoToEdit = values["-TODOS-"][0]
+            
+            i = todos.index(todoToEdit)
+            todos.pop(i)
+            
+            saveFile(filePath, todos)
+            
+            window['-TODOS-'].update(values=todos)
+            window['-TODO-'].update(value='')
+        except IndexError:
+            sg.popup("Select a task to Complete!", font=fonts)
+         
+            
+    elif event == "-TODOS-":
+        window['-TODO-'].update(value=values["-TODOS-"][0].strip())
+        
+    elif event == sg.WIN_CLOSED:
+        break
+
+window.close()
