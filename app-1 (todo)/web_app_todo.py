6f11aaf1c66700c65976e643af921b9de7541368
watch14 <watchman1414@gmail.com>
2024-08-18 15:56:10 +0100

    web app add task implamented

--- app-1 (todo)/web_app_todo.py
+++ app-1 (todo)/web_app_todo.py
@@ -1,9 +1,34 @@
 import streamlit as st
+from functions import *
+
+
+filePath = "todos.txt"
+todos = getTodos(filePath)
+time = currentTime()
+
+def addTodo():
+    todo = st.session_state["newTodo"]
+    todos.append(todo.strip().title() + "\n")
+    saveFile(filePath, todos)
+    st.session_state["newTodo"]=""
+
 
 st.write(""" 
          # My To-Do App
-         Task need to be done!
+         ###### impove your productivity!
          """)
 
+st.text_input(label="Add Task:", placeholder="Add Task:",
+                      label_visibility="collapsed", on_change=addTodo,
+                      key="newTodo"
+                      )
 
-st.checkbox("task 1")
+
+        
+st.write("Task need to be done!")
+
+for item in todos:
+    st.checkbox(item)
+    
+
+st.session_state
