0767dd574e96d2b92700f41fb20acd00253de4b0
watch14 <watchman1414@gmail.com>
2024-08-23 17:28:39 +0100

    fix pdf pages

--- app-4 (pdf template)/main.py
+++ app-4 (pdf template)/main.py
@@ -7,14 +7,14 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 content  = pd.read_csv("topics.csv")
 
 for index, item in content.iterrows():
+    pdf.add_page()
+    text = item["Topic"]
+    pdf.set_font("Arial", size=20, style="B")
+    pdf.cell(0, 12, txt=text, ln=1, align="L")
+    pdf.line(10, 22, 200, 22)
+    
     for i in range(item["Pages"]):
-        print(i)
         pdf.add_page()
-
-        text = item["Topic"]
-        pdf.set_font("Arial", size=20, style="B")
-        pdf.cell(0, 12, txt=text, ln=1, align="L")
-        pdf.line(10, 22, 200, 22)
 
 
 pdf.output("test.pdf")
