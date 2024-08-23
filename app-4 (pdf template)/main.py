f55840c9c76d3142a802fa4f31cdfb08c055adad
watch14 <watchman1414@gmail.com>
2024-08-23 17:25:00 +0100

    Refactor PDF generation code

--- app-4 (pdf template)/main.py
+++ app-4 (pdf template)/main.py
@@ -1,13 +1,20 @@
 from fpdf import FPDF
+import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+content  = pd.read_csv("topics.csv")
 
-pdf.set_font("Arial", size=20, style="B")
-pdf.cell(0, 12, txt="Hello World!", ln=1, align="L", border=1)
-pdf.cell(0, 12, txt="Data", ln=1, align="L", border=1)
+for index, item in content.iterrows():
+    for i in range(item["Pages"]):
+        print(i)
+        pdf.add_page()
+
+        text = item["Topic"]
+        pdf.set_font("Arial", size=20, style="B")
+        pdf.cell(0, 12, txt=text, ln=1, align="L")
+        pdf.line(10, 22, 200, 22)
 
 
 pdf.output("test.pdf")
